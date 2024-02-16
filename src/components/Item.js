83e1af473abf986b870a9ef0b560249d4135a030
JAMES MBUVI <completed assignment>
2024-02-16 12:21:31 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,57 @@
-import React from "react";
+// import React from "react";
 
-function Item({ item }) {
+// function Item({ item }) {
+//   return (
+//     <li className={item.isInCart ? "in-cart" : ""}>
+//       <span>{item.name}</span>
+//       <span className="category">{item.category}</span>
+//       <button className={item.isInCart ? "remove" : "add"}>
+//         {item.isInCart ? "Remove From" : "Add to"} Cart
+//       </button>
+//       <button className="remove">Delete</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
+
+
+
+function Item({ item, onUpdateItem }) {
+  // Add function to handle button click
+  function handleAddToCartClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+
+
+    
+    },
+    
+
+
+    body: JSON.stringify({
+      isInCart: !item.isInCart,
+    }),
+    
+  })
+    .then((r) => r.json())
+    .then((updatedItem) => onUpdateItem(updatedItem));
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      {/* add the onClick listener */}
+      <button
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
       <button className="remove">Delete</button>
     </li>
   );
 }
-
-export default Item;
